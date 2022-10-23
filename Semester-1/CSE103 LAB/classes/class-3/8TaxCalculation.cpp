ea787671667b802f5cf08e28865a5aa763359966
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-10-23 18:42:52 +0600

    🛠️solution 8 added

---
+++ Semester-1/CSE103 LAB/classes/class-3/8TaxCalculation.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int income;
+    float rate, tax;
+    printf("This program is to output calculated tax.\n");
+    printf("Enter the income: ");
+    scanf("%d", &income);
+    if (income <= 150000)
+    {
+        printf("You don't have to pay tax.");
+    }
+    else if (income > 150001 && income < 300000)
+    {
+        rate = 0.10;
+        tax = income * rate;
+        printf("The calculated tax is %f.", tax);
+    }
+    else if (income > 300001 && income < 500000)
+    {
+        rate = 0.20;
+        tax = income * rate;
+        printf("The calculated tax is %f.", tax);
+    }
+    else
+    {
+        rate = 0.30;
+        tax = income * rate;
+        printf("The calculated tax is %f.", tax);
+    }
+    return 0;
+}
