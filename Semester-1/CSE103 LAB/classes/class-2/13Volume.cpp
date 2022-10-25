cf21f571585cbfad2f3fd8d4c01367b8efe867fa
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-10-25 11:32:35 +0600

    🛠️formula correction

--- Semester-1/CSE103 LAB/classes/class-2/13Volume.cpp
+++ Semester-1/CSE103 LAB/classes/class-2/13Volume.cpp
@@ -8,7 +8,7 @@ int main()
     scanf("%d", &radius);
     printf("Input the length: ");
     scanf("%d", &length);
-    volumeR = (3 * 3.14 * radius * radius * radius) / 4;
+    volumeR = (4 * 3.14 * radius * radius * radius) / 3;
     volumeL = length * length * length;
     printf("Volume of sphere with a radius of %d unit is %f unit cube \nVolume of cube with a length of %d unit is %d unit cube", radius, volumeR, length, volumeL);
     return 0;
