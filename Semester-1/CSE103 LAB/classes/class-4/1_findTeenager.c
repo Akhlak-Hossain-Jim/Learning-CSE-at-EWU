b1657b96d04b824eb00bbe4abcb75dc4bf7594cb
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-10-30 17:38:12 +0600

    🛠️solution 1 added

---
+++ Semester-1/CSE103 LAB/classes/class-4/1_findTeenager.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, age, teens = 0;
+    printf("This program is to find number of teens.\n");
+    printf("Enter the number of peoples' age: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter person %d's age: ", i);
+        scanf("%d", &age);
+        if (age > 12 && age < 19)
+        {
+            teens += 1;
+        }
+    }
+    printf("%d of %d are teenagers.", teens, n);
+    return 0;
+}
