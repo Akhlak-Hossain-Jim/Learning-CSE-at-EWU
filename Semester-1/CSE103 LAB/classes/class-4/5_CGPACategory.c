c72648d28c63092b871d5e4618007acd206d04a8
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-10-30 18:12:19 +0600

    🛠️5no solution added

---
+++ Semester-1/CSE103 LAB/classes/class-4/5_CGPACategory.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, result = 0;
+    double cgpa;
+    printf("This program is to find group of people secured at least 3.5 CGPA.\n");
+    printf("Enter the number of students: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter person %d's CGPA: ", i + 1);
+        scanf("%lf", &cgpa);
+        if (3.5 <= cgpa)
+            result++;
+    }
+    printf("%d People secured at least 3.5 CGPA", result);
+    return 0;
+}
