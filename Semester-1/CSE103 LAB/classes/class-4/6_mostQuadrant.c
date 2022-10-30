bd34125183a04b5f94afe432f73add285542b2fd
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-10-30 22:00:48 +0600

    🛠️updated

--- Semester-1/CSE103 LAB/classes/class-4/6_mostQuadrant.c
+++ Semester-1/CSE103 LAB/classes/class-4/6_mostQuadrant.c
@@ -2,36 +2,41 @@
 
 int main()
 {
-    int n, x, y, i = 0, ii = 0, iii = 0, iv = 0, center = 0;
-    printf("This program is to find most poins falls in a quadrant of some 2D points.\n");
+    int n, x, y, first = 0, second = 0, third = 0, forth = 0, center = 0;
+    printf("This program is to find most points falls in a quadrant of some 2D points.\n");
     printf("Enter points to be compared: ");
     scanf("%d", &n);
-    for (int j = 0; j < n; j++)
+    if (n > 0)
     {
-        printf("Enter %d's x value: ", j + 1);
-        scanf("%lf", &x);
-        printf("Enter %d's y value: ", j + 1);
-        scanf("%lf", &y);
-        if (x == 0 && y == 0)
-            center++;
-        else if (x > 0 && y > 0)
-            i++;
-        else if (x < 0 && y > 0)
-            ii++;
-        else if (x < 0 && y < 0)
-            iii++;
-        else if (x > 0 && y < 0)
-            iv++;
+        for (int i = 0; i < n; i++)
+        {
+            printf("Enter %d's x value: ", i + 1);
+            scanf("%lf", &x);
+            printf("Enter %d's y value: ", i + 1);
+            scanf("%lf", &y);
+            if (x == 0 && y == 0)
+                center++;
+            else if ((x > 0 && y > 0) || (x > 0 && y == 0) || (x == 0 && y > 0))
+                first++;
+            else if ((x < 0 && y > 0) || (x < 0 && y == 0))
+                second++;
+            else if (x < 0 && y < 0)
+                third++;
+            else if ((x > 0 && y < 0) || (x == 0 && y < 0))
+                forth++;
+        }
+        if (center > first && center > second && center > third && center > forth)
+            printf("Most points are in the center.");
+        else if (first > center && first > second && first > third && first > forth)
+            printf("Most points are in 1st quadrant.");
+        else if (second > center && second > first && second > third && second > forth)
+            printf("Most points are in 2nd quadrant.");
+        else if (third > center && third > first && third > second && third > forth)
+            printf("Most points are in 3rd quadrant.");
+        else if (forth > center && forth > first && forth > second && forth > third)
+            printf("Most points are in 4th quadrant.");
     }
-    if (center > i && center > ii && center > iii && center > iv)
-        printf("Most points are in center quadrant.");
-    else if (i > center && i > ii && i > iii && i > iv)
-        printf("Most points are in 1st quadrant.");
-    else if (ii > center && ii > i && ii > iii && ii > iv)
-        printf("Most points are in 2nd quadrant.");
-    else if (iii > center && iii > i && iii > ii && iii > iv)
-        printf("Most points are in 3rd quadrant.");
     else
-        printf("Most points are in 4th quadrant.");
+        printf("Can't compare less then 1 2D point");
     return 0;
 }
