fe73b03569f44f99f692f213f1ef09e65ef64210
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-10-30 17:47:24 +0600

    🪲fix

--- Semester-1/CSE103 LAB/classes/class-4/1_findTeenager.c
+++ Semester-1/CSE103 LAB/classes/class-4/1_findTeenager.c
@@ -2,15 +2,19 @@
 
 int main()
 {
-    int n, age, teens = 0;
+    int n, age, child = 0, teens = 0, senior = 0;
     printf("This program is to find number of teens.\n");
     printf("Enter the number of peoples' age: ");
     scanf("%d", &n);
     for (int i = 0; i < n; i++)
     {
-        printf("Enter person %d's age: ", i);
+        printf("Enter person %d's age: ", i + 1);
         scanf("%d", &age);
-        if (age > 12 && age < 20)
+        if (age > 12 && age < 19)
+        {
+            teens += 1;
+        }
+        else if (age > 12 && age < 19)
         {
             teens += 1;
         }
