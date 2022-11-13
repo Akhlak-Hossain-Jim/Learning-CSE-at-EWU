69aa0c263d5abbea1252be61a4c5a21bc0af2c66
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-11-13 17:50:30 +0600

    🛠️2 no solution added

---
+++ Semester-1/CSE103 LAB/classes/class-5/2-sumOfTheArray.c
@@ -0,0 +1,23 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main()
+{
+    int n, sum = 0;
+    int arr[100];
+    printf("This program is to find the sum of an array.\nEnter the array length: ");
+    scanf("%d", &n);
+    if (n < 101)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            printf("Enter the value for %d place in array: ", i);
+            scanf("%d", &arr[i]);
+            sum += arr[i];
+        }
+        printf("The sum of the array is: %d", sum);
+    }
+    else
+        printf("Can't consider more than 100 length of array.");
+    return 0;
+}
