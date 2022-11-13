b5e7c01f33de4538fcf93d1ca4260548e60c49b8
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-11-13 17:43:22 +0600

    🛠️solution for 1 added

---
+++ Semester-1/CSE103 LAB/classes/class-5/1-linear search.c
@@ -0,0 +1,33 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main()
+{
+    int n, search, result;
+    int arr[100];
+    printf("This program is to find a number from an array by linear search\nEnter the array length: ");
+    scanf("%d", &n);
+    if (n < 101)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            printf("Enter the value for %d place in array: ", i);
+            scanf("%d", &arr[i]);
+        }
+        printf("Enter the value to find: ");
+        scanf("%d", &search);
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] == search)
+            {
+                printf("Found the %d value at the index of %d in the array.", search, i);
+                break;
+            }
+            else if (arr[i] != search && i == n - 1)
+                printf("No item found as %d.", search);
+        }
+    }
+    else
+        printf("Can't consider more than 100 length of array.");
+    return 0;
+}
