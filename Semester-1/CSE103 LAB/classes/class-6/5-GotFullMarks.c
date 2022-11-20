8cf989eed5214cc393dba26e6579460dc3a7b558
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-11-20 19:00:50 +0600

    🪲fix

--- Semester-1/CSE103 LAB/classes/class-6/5-GotFullMarks.c
+++ Semester-1/CSE103 LAB/classes/class-6/5-GotFullMarks.c
@@ -2,16 +2,16 @@
 
 int main()
 {
-    int temps[100], n, state = 0;
+    int results[100], n, state = 0;
     printf("This program  can input some students' marks and check whether any student got full marks. You may assume that exam's total mark is 30.\nEnter the array length: ");
     scanf("%d", &n);
     if (n < 101)
     {
         for (int i = 0; i < n; i++)
         {
-            printf("Enter day %d's temperature: ", i + 1);
-            scanf("%d", &temps[i]);
-            if (temps[i] == 30)
+            printf("Enter the result of person %d: ", i + 1);
+            scanf("%d", &results[i]);
+            if (results[i] == 30)
             {
                 state++;
             }
