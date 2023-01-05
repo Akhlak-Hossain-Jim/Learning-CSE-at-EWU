5e3d3bf07e773422f3de86e0300a7b9fb3b2ef17
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-01-06 00:57:49 +0600

    🛠️📕added ordering flow

---
+++ Projects/CSE103/CoffeeShopManagement/main.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+struct menu
+{
+    int code;
+    char name[40];
+    double price;
+    int inStock;
+};
+struct sold
+{
+    int code;
+    char name[40];
+    double price;
+    int quantity;
+};
+
+int menuLength = 8;
+
+struct menu COFFEEs[20] = {
+    {101, "Americano  ", 120.4, 23},
+    {102, "Cappuccino ", 100.4, 30},
+    {103, "Espresso   ", 110.4, 20},
+    {104, "Macchiato  ", 101.4, 33},
+    {105, "Mocaccino  ", 130, 55},
+    {106, "Latte      ", 135, 20},
+    {107, "Cold Coffee", 103, 30},
+    {108, "Affogato   ", 150, 10},
+};
+
+void info()
+{
+    printf("Loading................\n\n\n\n");
+    printf("\t\tWELCOME to COFFEE SHOP MANAGEMENT APP\n");
+    printf("\n%c\t\t\tCreated by\n\n", ' ');
+    printf("%c\t\tName:\tAklhak Hossain\t\tId:\t2022-3-60-057\n", ' ');
+    printf("%c\t\tName:\tSaba Tasnim Khan\tId:\t2022-3-60-049\n", ' ');
+    printf("%c\t\tName:\tHasnay Hasin\t\tId:\t2022-3-60-038\n", ' ');
+    printf("%c\t\tName:\tAnika Tahseen\t\tId:\t2022-3-60-037\n", ' ');
+    return;
+}
+
+void selectBasicOptionInfo()
+{
+    printf("\n\n\n \t\tSelect an Option:\n");
+    printf("%c\t\t\t1. Print Item list.\n", ' ');
+    printf("%c\t\t\t9. Exit\n", ' ');
+    return;
+}
+
+void printList()
+{
+    printf("\n\n");
+    printf("\t\t-------------------------------------------------\n\t\t| ID  | Name \t\t| Price \t| Available |\n \t\t-------------------------------------------------\n");
+    for (int i = 0; i < menuLength; i++)
+    {
+        printf("\t\t| %d | %s\t| %.2lf\t| %d \t\t|\n", COFFEEs[i].code, COFFEEs[i].name, COFFEEs[i].price, COFFEEs[i].inStock);
+        printf(" \t\t-------------------------------------------------\n");
+    }
+    return;
+}
+
+int main()
+{
+    int identifier = 8, itemID, quantity;
+    double itemPrice;
+    char itemName[100];
+    info();
+    selectBasicOptionInfo();
+    printf("\n \t\tSelect: ");
+    scanf("%d", &identifier);
+
+    while (identifier)
+    {
+        if (identifier == 1)
+        {
+            int staged;
+            printList();
+            printf("\n \t\tPlace an order\n\t\t\tEnter Item ID or 9 to Exit or 8 to go to Main Menu\n\t\t\tItem ID: ");
+            scanf("%d", &staged);
+            if (staged == 9)
+                identifier = staged;
+            else if (staged == 8)
+                identifier = 8;
+            else
+            {
+                identifier = 10;
+                itemID = staged;
+            }
+        }
+        else if (identifier == 10)
+        {
+            int i, staged;
+            for (i = 0; i < menuLength; i++)
+            {
+                if (COFFEEs[i].code == itemID && COFFEEs[i].inStock > 0)
+                {
+                    printf("\n\n\t\t %s withCode %d is Available(%d) at price: %.2lf\n\t\t\tEnter Quantity to confirm or 0 to cancel: ", COFFEEs[i].name, COFFEEs[i].code, COFFEEs[i].inStock, COFFEEs[i].price);
+                    scanf("%d", &staged);
+                    if (staged == 0)
+                        identifier = 8;
+                    else if (COFFEEs[i].inStock >= staged)
+                    {
+                        COFFEEs[i].inStock -= staged;
+                        quantity = staged;
+                        itemPrice = COFFEEs[i].price;
+                        strcpy(itemName, COFFEEs[i].name);
+                        identifier = 11;
+                    }
+                    else
+                    {
+                        printf("\t\tQuantity is not availabe.\n");
+                        identifier = 8;
+                    }
+                    break;
+                }
+            }
+            if (i == menuLength)
+            {
+                printf(" \t\tNo item found. Enter a valid itemId.\n\n\n\n");
+                identifier = 8;
+            }
+        }
+        else if (identifier == 11)
+        {
+            printf("\n\n\t\t\t\tBill:\n");
+            printf("\t\tName    : %s\n\t\tQuantity: %d\n\t\tPrice   :  %.2lf\n\----------------------\n\t\tTotal   : %.2lf\n\n", itemName, quantity, itemPrice, quantity * itemPrice);
+            identifier = 8;
+        }
+        else if (identifier == 8)
+        {
+            printf("\n\n\n");
+            selectBasicOptionInfo();
+            printf("\n \t\tSelect: ");
+            scanf("%d", &identifier);
+        }
+        else if (identifier == 9)
+        {
+            printf("Closing app.......");
+            break;
+        }
+    }
+    return 0;
+}
