187a7ebb73a3a8d18f9a49a0b44603672fb21499
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-04-06 12:51:49 +0600

    🛠️added

---
+++ functions/WhichCourseCanITake.js
@@ -0,0 +1,128 @@
+let data = `1	ACT101	Financial Accounting	63	140	0	0	
+2	BUS231	Business Communication	63	140	0	0	ENG102
+3	BUS321	Business for Engineering and Technology	63	140	0	0	ENG102
+4	CHE109	Engineering Chemistry-I	3	140	0	0	
+5	CSE103	Structured Programming	0	140	0	0	
+6	CSE106	Discrete Mathematics	3	140	0	0	CSE103
+7	CSE110	Object Oriented Programming	13	140	0	0	CSE106
+8	CSE200	Computer-Aided Engineering Drawing	23	140	0	0	
+9	CSE207	Data Structures	33	140	0	0	CSE110
+10	CSE209	Electrical Circuits	23	140	0	0	
+11	CSE225	Numerical Methods	90	140	0	0	CSE103
+12	CSE246	Algorithms	57	140	0	0	
+13	CSE251	Electronic Circuits	33	140	0	0	CSE209
+14	CSE302	Database Systems	57	140	0	0	CSE106
+15	CSE313	Theory of Computations	90	140	0	0	CSE246
+16	CSE325	Operating Systems	45	140	0	0	CSE207
+17	CSE345	Digital Logic Design	67	140	0	0	CSE251
+18	CSE347	Information System Analysis and Design	67	140	0	0	CSE302
+19	CSE350	Data Communications	67	140	0	0	CSE251
+20	CSE355	Digital System Design	67	140	0	0	CSE345
+21	CSE360	Computer Architecture	67	140	0	0	CSE325
+22	CSE366	Artificial Intelligence	75	140	0	0	CSE246
+23	CSE400	Capstone Project	95	140	0	0	
+24	CSE405	Computer Networks	75	140	0	0	CSE246
+25	CSE406	Internet of Things	97	140	0	0	CSE405
+26	CSE412	Software Engineering	73	140	0	0	CSE347
+27	CSE420	Computer Graphics	97	140	0	0	CSE246
+28	CSE422	Simulation and Modeling	97	140	0	0	CSE246
+29	CSE423	Software Architecture	97	140	0	0	CSE412
+30	CSE428	Human Computer Interactions	97	140	0	0	CSE412
+31	CSE430	Software Testing and Quality Assurance	85	140	0	0	CSE412
+32	CSE432	Digital Signal Processing	85	140	0	0	CSE246
+33	CSE438	Digital Image Processing	97	140	0	0	CSE246
+34	CSE442	Microprocessors and Microcontrollers	85	140	0	0	CSE360
+35	CSE452	Distributed Systems and Algorithms	97	140	0	0	CSE325
+36	CSE453	Wireless Network	97	140	0	0	CSE405
+37	CSE457	Cellular Networks	97	140	0	0	CSE405
+38	CSE460	Cryptography	97	140	0	0	CSE246
+39	CSE464	Advanced Database System	97	140	0	0	CSE302
+40	CSE471	Compiler Design	97	140	0	0	CSE246
+41	CSE474	Pattern Recognition	97	140	0	0	CSE366
+42	CSE475	Machine Learning	97	140	0	0	CSE366
+43	CSE477	Data Mining	97	140	0	0	CSE366
+44	CSE479	Web Programming	97	140	0	0	CSE302
+45	CSE483	Graph Theory	97	140	0	0	CSE246
+46	CSE484	Computational Geometry	97	140	0	0	CSE246
+47	CSE486	Bioinformatics Algorithms	97	140	0	0	CSE246
+48	CSE487	Computer and Cyber Security	97	140	0	0	CSE405
+49	CSE489	Mobile Programming	75	140	0	0	CSE246
+50	CSE491	VLSI Design	97	140	0	0	CSE345
+51	CSE492	Robotics	97	140	0	0	CSE366
+52	CSE494	Embedded Systems	97	140	0	0	CSE442
+53	ECO101	Principles of Microeconomics	38	140	0	0	
+54	ENG101	Basic English	0	140	0	0	
+55	ENG102	Composition and Communication Skills	3	140	0	0	ENG101
+56	FIN101	Principles of Finance	63	140	0	0	STA102
+57	GEN201	Bangladesh Studies	49	140	0	0	ENG102
+58	GEN202	Eastern Culture and Heritage	140	140	0	0	
+59	GEN203	Ecological System and Environment	38	140	0	0	
+60	GEN204	Western Thought	49	140	0	0	
+61	GEN205	Introduction to Psychology	140	140	0	0	
+62	GEN207	Industrial Psychology	140	140	0	0	
+63	GEN208	Introduction to Philosophy	140	140	0	0	
+64	GEN209	Social Psychology	140	140	0	0	
+65	GEN210	International Relations	49	140	0	0	ENG102
+66	GEN211	Concepts of Journalism & Media Studies	140	140	0	0	
+67	GEN212	Women in Development	140	140	0	0	
+68	GEN213	Introduction to German Language	140	140	0	0	
+69	GEN214	Introduction to Development Studies	38	140	0	0	ENG102
+70	GEN215	Introduction to French Language	140	140	0	0	
+71	GEN216	Introduction to Spanish Language	140	140	0	0	
+72	GEN217	Introduction to Chinese Language	140	140	0	0	
+73	GEN218	Introduction to Arabic Language	140	140	0	0	
+74	GEN220	Principles of Public Relations	140	140	0	0	
+75	GEN221	Globalization, Development and Change	140	140	0	0	
+76	GEN223	Contemporary Security Studies in Asia-Pacific	140	140	0	0	
+77	GEN224	Bengali Language	140	140	0	0	
+78	GEN226	Emergence of Bangladesh	26	140	0	0	ENG102
+79	GEN239	Professional Ethics	140	140	0	0	
+80	MAT101	Differential & Integral Calculus	0	140	0	0	
+81	MAT102	Differential Equations & Special Functions	3	140	0	0	MAT101
+82	MAT104	Co-ordinate Geometry & Vector Analysis	13	140	0	0	MAT101
+83	MAT205	Linear Algebra & Complex Variables	38	140	0	0	MAT102
+84	MGT321	Industrial Management	140	140	0	0	ENG102
+85	MGT337	Production Operations Management	63	140	0	0	STA102
+86	MKT101	Principles of Marketing	63	140	0	0	
+87	PHY109	Engineering Physics-I	13	140	0	0	MAT102
+88	PHY209	Engineering Physics-II	49	140	0	0	MAT205
+89	POP201	Health Challenges of Adolescents and Youths	150	150	0	0	
+90	POP202	Introduction to Public Health	150	150	0	0	
+91	POP203	Introduction to Population Studies	150	150	0	0	
+92	SOC217	Religion, Ethnicity, Culture and Development in South Asia	49	140	0	0	ENG102
+93	SOC317	Sociology of Science and Technology	38	140	0	0	
+94	STA102	Statistics and Probability	26	140	0	0	
+95	CSE407	Green Computing	97	140	0	0	CSE405
+96	CSE495	IT Project Management and Entrepreneurship	110	140	0	0	CSE347
+97	CSE326	Algorithms	63	140	0	0	CSE207
+98	CSE303	Statistics for Data Science	73	140	0	0	STA102
+99	CSE445	Computer Vision	97	140	0	0	CSE246
+100	CSE481	Nature-Inspired Computing	97	140	0	0	CSE246
+101	CSE488	Big Data Analytics	97	140	0	0	CSE302
+102	CSE472	Advanced Network Services and Management	97	140	0	0	CSE405
+103	CSE473	Network Security and Systems	97	140	0	0	CSE405
+104	CSE446	ASIC Design Using FPGA	97	140	0	0	CSE345
+105	CSE400A	Capstone Project (Part 1 of 3)	95	140	0	0	
+106	CSE400B	Capstone Project (Part 2 of 3)	95	140	0	0	
+107	CSE400C	Capstone Project (Part 3 of 3)	95	140	0	0	`;
+
+function processData(data) {
+  let arrA = data.split("\n");
+  let arrB = [...arrA].map((el) => {
+    let arr = el.split("\t");
+    let obj = {
+      id: arr[0],
+      code: arr[1],
+      sub: arr[1].substring(0, 3),
+      name: arr[2],
+      minCreditEarned: arr[3],
+      maxCreditEarned: arr[4],
+      minCGPA: arr[5],
+      maxCGPA: arr[6],
+      courseRequired: arr[7],
+    };
+    return obj;
+  });
+  return arrB;
+}
+console.log(processData(data));
