519bfdce58b2a009303ad919eaeb2d91ceffe066
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-06-12 13:02:38 +0600

    CSE110 lab works completed

---
+++ Semester-3/CSE110 Lab/Lab 2/isDivisable_1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class IsDivisible {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner (System.in);
+        System.out.print("This program is finding if the given number is divisible by 2 or 3.\nEnter the number: ");
+        int num = inp.nextInt();
+        if ((num%2==0 || num%3==0)&&!(num%2==0 && num%3==0))
+            System.out.print("TRUE");
+        else
+            System.out.print("FALSE");
+    }
+}
---
+++ Semester-3/CSE110 Lab/Lab 2/takingInput_0.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class SampleReadInput {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner (System.in);
+        System.out.print("Enter your name: ");
+        String name = inp.nextLine();
+        System.out.print("Enter your age: ");
+        int age = inp.nextInt();
+        System.out.print("Enter your CGPA: ");
+        double cg = inp.nextDouble();
+        System.out.print("Enter your department: ");
+        inp.nextLine();
+        String department = inp.nextLine();
+        
+        System.out.println();
+        
+        System.out.println("Your Name: "+name);
+        System.out.println("Your Age: "+age);
+        System.out.println("Your CGPA: "+cg);
+        System.out.println("Your Department: "+department);
+    }
+}
