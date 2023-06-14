fe7aca6a819c6ef1d2ea64bd04bf98509e51cf3f
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-06-14 15:41:32 +0600

    📕added HW 2

--- Semester-3/CSE110/Homework about array/ArrayWithRandomValue.java
+++ Semester-3/CSE110/Homework about array/ArrayWithRandomValue.java
@@ -2,10 +2,16 @@ import java.util.Scanner;
 
 class RandomValues {
     public static void main(String[] args) {
-        System.out.println("This program is about Initializing arrays with random values");
-        int [] array={1,2,3,4,5};
-        for(int i =0;i<array.length;i++){
-            System.out.println(array[i]);
+        System.out.print("This program is about Initializing arrays with random values.\nEnter the array length:");
+        Scanner inp = new Scanner (System.in);
+        int n=inp.nextInt();
+        int [] array = new int[n];
+        for(int i =0;i<n;i++){
+            array[i]=(int)(Math.random()*100);
+        }
+        System.out.println("The randomly generated array is:");
+        for(int el: array){
+            System.out.println(el);
         }
     }
 }
