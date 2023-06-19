29543bf7f5fcbce9437ecec7793eb20b5ac18357
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-06-19 12:34:46 +0600

    🛠️lab 3 prob 4 solution added

---
+++ Semester-3/CSE110 Lab/Lab3/UniqueNumber.java
@@ -0,0 +1,34 @@
+// package lab3;
+import java.util.Scanner;
+
+class UniqueNumber {
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+        System.out.print("This program is printing unique values.\nEnter 10 numbers: ");
+        int[] arr = new int[10];
+        int[] set = new int[10];
+        int i;
+        int j = 0;
+        for (i=0;i<10;i++){
+            int x =inp.nextInt();
+            arr[i]=x;
+            int f = 0;
+            for (int el: set){
+                if(arr[i]==el){
+                    f=1;
+                    break;
+                }
+            }
+            if(f==0){
+                set[j]=arr[i];
+                j++;
+            }
+            i++;
+        }
+        System.out.print("Unique inputs: ");
+        for(int el: set){
+            if (el!=0)
+                System.out.print(el+" ");
+        }
+    }
+}
