9531529aa9bc8e480c06cbb2900d0ca185d0e3c5
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-06-19 12:55:39 +0600

    🛠️lab 3 prob 5 solution added

---
+++ Semester-3/CSE110 Lab/Lab3/RanBigSmall.java
@@ -0,0 +1,40 @@
+package lab3;
+import java.util.Scanner;
+
+class RanBigSmall {
+    static void maximum(int [] arr){
+        int max = arr[0];
+        int in = 0;
+        for(int i=0;i<arr.length;i++){
+            if (arr[i]>max){
+                in=i;
+                max=arr[i];
+            }
+        }
+        System.out.println("The highest element is "+max+" with index of "+in);
+    }
+    static void minimum(int [] arr){
+        int min = arr[0];
+        int in = 0;
+        for(int i=0;i<arr.length;i++){
+            if (arr[i]<min){
+                in=i;
+                min=arr[i];
+            }
+        }
+        System.out.println("The smallest element is "+min+" with index of "+in);
+    }
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+        System.out.println("This program is generating a random array with 100 elements and finding the highest and smallest value with index.");
+        int[] arr = new int[100];
+        System.out.println("Generating a random array with 100 elements.");
+        for (int i=0;i<100;i++){
+            int x =(int)(Math.random()*100);
+            arr[i]=x;
+        }
+        System.out.println("A random array with 100 elements is successfully generated.\nFinding the highest and smallest value with index.");
+        maximum(arr);
+        minimum(arr);
+    }
+}
