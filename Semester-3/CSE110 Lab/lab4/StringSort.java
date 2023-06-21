2786f590153e655cc0de0abbd67fae709978d559
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-06-21 21:44:36 +0600

    🛠️lab 4 solutions added

---
+++ Semester-3/CSE110 Lab/lab4/StringSort.java
@@ -0,0 +1,22 @@
+package lab4;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class StringSort{
+    public static void main(String[] args){
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter the number of elements you wanna sort: ");
+        int n = inp.nextInt();
+        String[] arr=new String[n];
+        inp.nextLine();
+        for(int i = 0;i<n;i++){
+            System.out.print("Enter "+i+"th string: ");
+            arr[i]=inp.nextLine();
+        }
+        Arrays.sort(arr);
+        for(String el: arr){
+            System.out.println(el);
+        }
+    }
+}
