ed948c361a8474c813fe685ade4581d963c242e7
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-07-21 14:35:40 +0600

    🛠️🛠️lab 5 completed

---
+++ Semester-3/CSE110 Lab/lab5/BookManipulation.java
@@ -0,0 +1,79 @@
+package lab5;
+
+import java.util.Scanner;
+
+class Book {
+    private int isbn, numberOfPages;
+    private static int count;
+    String bookTitle;
+
+    public Book(int is, String ti, int n) {
+        this.isbn = is;
+        this.bookTitle = ti;
+        this.numberOfPages = n;
+        updateCount();
+    }
+
+    public Book() {
+        updateCount();
+    }
+
+    static private void updateCount() {
+        count++;
+    }
+
+    String display() {
+        return "Book Name: " + bookTitle + "\nISBN: " + isbn + "\nNumber of Pages: " + numberOfPages;
+    }
+
+    int compareTo(Book b) {
+        if (this.numberOfPages > b.numberOfPages)
+            return 1;
+        else if (this.numberOfPages == b.numberOfPages)
+            return 0;
+        else
+            return -1;
+    }
+
+    static int getCount() {
+        return count;
+    }
+
+    int getNumberOfPages() {
+        return numberOfPages;
+    }
+}
+
+public class BookManipulation {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter the total number of book information you want to add: ");
+        int n = inp.nextInt();
+        Book[] bookArray = new Book[n];
+        for (int i = 0; i < bookArray.length; i++) {
+            inp.nextLine();
+            System.out.print("Enter the " + (i + 1) + "th Book's name: ");
+            String na = inp.nextLine();
+            System.out.print("Enter the " + (i + 1) + "th Book's ISBN: ");
+            int isbn = inp.nextInt();
+            System.out.print("Enter the " + (i + 1) + "th Book's total pages: ");
+            int pages = inp.nextInt();
+            bookArray[i] = new Book(isbn, na, pages);
+        }
+        displayAll(bookArray);
+        System.out.println("Compareing 1st and second book page information.");
+        System.out.println(bookArray[0].compareTo(bookArray[1]));
+        System.out.println("Chaking if the 3rd book is heavier.");
+        System.out.println(isHeavier(bookArray[3]));
+    }
+
+    static void displayAll(Book[] ba) {
+        for (int i = 0; i < ba.length; i++) {
+            System.out.println(ba[i].display());
+        }
+    }
+
+    static boolean isHeavier(Book b) {
+        return b.getNumberOfPages() > 500;
+    }
+}
---
+++ Semester-3/CSE110 Lab/lab5/Fractionalization.java
@@ -0,0 +1,82 @@
+package lab5;
+
+import java.util.Scanner;
+
+class Fraction {
+    private int numerator, denominator;
+
+    String sumO, subO, mulO, divO;
+
+    public Fraction(int nu, int de) {
+        this.numerator = nu;
+        this.denominator = de;
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+
+    public String display() {
+        return numerator + " / " + denominator;
+    }
+
+    public void add(Fraction f) {
+        this.subO = (this.numerator * f.getDenominator()) + (f.getNumerator() * denominator) + " / "
+                + (denominator * f.getDenominator());
+    }
+
+    public void sub(Fraction f) {
+        this.subO = ((this.numerator * f.getDenominator()) - (f.getNumerator() * denominator)) + " / "
+                + (denominator * f.getDenominator());
+    }
+
+    public void multiplication(Fraction f) {
+        this.mulO = ((this.numerator * f.getNumerator())) + " / " + (denominator * f.getDenominator());
+    }
+
+    public void division(Fraction f) {
+        divO = ((this.numerator * f.getDenominator())) + " / " + (denominator * f.getNumerator());
+    }
+}
+
+public class Fractionalization {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter the numerator for the 1st fraction: ");
+        int n = inp.nextInt();
+        System.out.print("Enter the denominator for the 1st fraction: ");
+        int d = inp.nextInt();
+        Fraction f1 = new Fraction(n, d);
+        System.out.print("Enter the numerator for the 2nd fraction: ");
+        n = inp.nextInt();
+        System.out.print("Enter the denominator for the 2nd fraction: ");
+        d = inp.nextInt();
+        Fraction f2 = new Fraction(n, d);
+        System.out.println(f1.display());
+        System.out.println(f2.display());
+        System.out.println("Calculating Sum of 1st & 2nd fraction");
+        f1.add(f2);
+        System.out.println(f1.sumO);
+        System.out.println("Calculating Subtraction of 1st & 2nd fraction");
+        f1.sub(f2);
+        System.out.println(f1.subO);
+        System.out.println("Calculating Multiplication of 1st & 2nd fraction");
+        f1.multiplication(f2);
+        System.out.println(f1.mulO);
+        System.out.println("Calculating Division of 1st & 2nd fraction");
+        f1.division(f2);
+        System.out.println(f1.divO);
+    }
+}
