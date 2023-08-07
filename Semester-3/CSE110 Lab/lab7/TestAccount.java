4f79a32dc124e7abfa9af6032a33f7c42922c0b6
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-08-07 10:18:04 +0600

    🛠️updated all untested

--- Semester-3/CSE110 Lab/lab7/TestAccount.java
+++ Semester-3/CSE110 Lab/lab7/TestAccount.java
@@ -2,6 +2,7 @@ package lab7;
 
 import java.util.Scanner;
 import java.util.Calendar;
+import java.util.ArrayList;
 
 class Account {
     private int ID = 0;
@@ -159,26 +160,159 @@ public class TestAccount {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
 
+        ArrayList<Account> Accounts = new ArrayList<>();
+
         System.out.println("Welcome to XYZ Bank Management.");
 
         int shifter = 30;
         while (shifter != -1) {
             switch (shifter) {
                 case 1:
-
+                    System.out.println("\n\n\n\n");
+                    System.out.println("Creating a Checking Account, Enter below details:\n");
+                    System.out.print("Enter Checking Account's ID: ");
+                    int caID = inp.nextInt();
+                    System.out.print("Enter Checking Account's Initial Balance: ");
+                    double caBalance = inp.nextDouble();
+                    System.out.print("Enter Checking Account's Annual Interest Rate: ");
+                    double caAIR = inp.nextDouble();
+                    System.out.print("Enter Checking Account's overdraft limit: ");
+                    double caLim = inp.nextDouble();
+                    Accounts.add(new CheckingAccount(caID, caBalance, caAIR, caLim));
+                    System.out.println("\n\nChecking account with the ID of " + caID
+                            + " successfully created.\nGetting back to main menu.");
+                    shifter = 30;
                     break;
-
+                case 2:
+                    System.out.println("\n\n\n\n");
+                    System.out.println("Creating a Savings Account, Enter below details:\n");
+                    System.out.print("Enter Savings Account's ID: ");
+                    int saID = inp.nextInt();
+                    System.out.print("Enter Savings Account's Initial Balance: ");
+                    double saBalance = inp.nextDouble();
+                    System.out.print("Enter Savings Account's Annual Interest Rate: ");
+                    double saAIR = inp.nextDouble();
+                    Accounts.add(new SavingAccount(saID, saBalance, saAIR));
+                    System.out.println("\n\nSavings account with the ID of " + saID
+                            + " successfully created.\nGetting back to main menu.");
+                    shifter = 30;
+                    break;
+                case 3:
+                    System.out.println("\n\n\n\n");
+                    System.out.println("Depositing to a Checking Account.\n");
+                    System.out.print("Enter Checking Account's ID to search: ");
+                    int caIDd = inp.nextInt();
+                    boolean cadCan = false;
+                    for (int i = 0; i < Accounts.size(); i++) {
+                        if (Accounts.get(i).getID() == caIDd) {
+                            System.out.print("Enter amount to deposit: ");
+                            double dbl = inp.nextDouble();
+                            Accounts.get(i).deposit(dbl);
+                            cadCan = true;
+                            System.out.println(
+                                    "\n\nSuccessfully deposited amount " + dbl + " to Checking account: " + caIDd
+                                            + " new balance: " + Accounts.get(i).getBalance()
+                                            + ".\nGetting back to main menu.");
+                            break;
+                        }
+                    }
+                    if (!cadCan) {
+                        System.out.println("\n\nNo Account found with the ID of " + caIDd
+                                + ".\nGetting back to main menu.");
+                    }
+                    shifter = 30;
+                    break;
+                case 4:
+                    System.out.println("\n\n\n\n");
+                    System.out.println("Depositing to a Saving Account.\n");
+                    System.out.print("Enter Saving Account's ID to search: ");
+                    int saIDd = inp.nextInt();
+                    boolean sadCan = false;
+                    for (int i = 0; i < Accounts.size(); i++) {
+                        if (Accounts.get(i).getID() == saIDd) {
+                            System.out.print("Enter amount to deposit: ");
+                            double dbl = inp.nextDouble();
+                            Accounts.get(i).deposit(dbl);
+                            cadCan = true;
+                            System.out.println(
+                                    "\n\nSuccessfully deposited amount " + dbl + " to Saving account: " + saIDd
+                                            + " new balance: " + Accounts.get(i).getBalance()
+                                            + ".\nGetting back to main menu.");
+                            break;
+                        }
+                    }
+                    if (!sadCan) {
+                        System.out.println("\n\nNo Account found with the ID of " + saIDd
+                                + ".\nGetting back to main menu.");
+                    }
+                    shifter = 30;
+                    break;
+                case 5:
+                    System.out.println("\n\n\n\n");
+                    System.out.println("Making withdraw from a Checking Account.\n");
+                    System.out.print("Enter Checking Account's ID to search: ");
+                    int caIDw = inp.nextInt();
+                    boolean cawCan = false;
+                    for (int i = 0; i < Accounts.size(); i++) {
+                        if (Accounts.get(i).getID() == caIDw) {
+                            System.out.print("Enter amount to withdraw: ");
+                            double dbl = inp.nextDouble();
+                            Accounts.get(i).withdraw(dbl);
+                            cawCan = true;
+                            System.out.println(
+                                    "\n\nSuccessfully withdrawn amount " + dbl + " from Checking account: " + caIDw
+                                            + " new balance: " + Accounts.get(i).getBalance()
+                                            + ".\nGetting back to main menu.");
+                            break;
+                        }
+                    }
+                    if (!cawCan) {
+                        System.out.println("\n\nNo Account found with the ID of " + caIDw
+                                + ".\nGetting back to main menu.");
+                    }
+                    shifter = 30;
+                    break;
+                case 6:
+                    System.out.println("\n\n\n\n");
+                    System.out.println("Making withdraw from a Saving Account.\n");
+                    System.out.print("Enter Saving Account's ID to search: ");
+                    int saIDw = inp.nextInt();
+                    boolean sawCan = false;
+                    for (int i = 0; i < Accounts.size(); i++) {
+                        if (Accounts.get(i).getID() == saIDw) {
+                            System.out.print("Enter amount to withdraw: ");
+                            double dbl = inp.nextDouble();
+                            Accounts.get(i).deposit(dbl);
+                            cadCan = true;
+                            System.out.println(
+                                    "\n\nSuccessfully withdrawn amount " + dbl + " From Savings account: " + saIDw
+                                            + " new balance: " + Accounts.get(i).getBalance()
+                                            + ".\nGetting back to main menu.");
+                            break;
+                        }
+                    }
+                    if (!sawCan) {
+                        System.out.println("\n\nNo Account found with the ID of " + saIDw
+                                + ".\nGetting back to main menu.");
+                    }
+                    shifter = 30;
+                    break;
+                case 7:
+                    for (int i = 0; i < Accounts.size(); i++) {
+                        Accounts.get(i).display();
+                    }
+                    shifter = 30;
+                    break;
                 default:
-                    System.out.println(
+                    System.out.print(
                             "\n\n\n\n\t\tPress (1) for creating a Checking Account\n" +
                                     "\t\tPress (2) for creating a Savings Account\n" +
                                     "\t\tPress (3) for Deposit to a Checking Account\n" +
                                     "\t\tPress (4) for Deposit to a Savings Account\n" +
                                     "\t\tPress (5) for Withdraw from a Checking Account\n" +
                                     "\t\tPress (6) for Withdraw from a Savings Account\n" +
-                                    "\t\tPress (7) for Display all Accounts\n" +
-                                    "\t\tPress (8) for Deposit to a Checking Account\n" +
-                                    "\t\tPress (9) for Deposit to a Savings Account\n");
+                                    "\t\tPress (7) for Display all Accounts\n\nChoose an option to continue: ");
+                    shifter = inp.nextInt();
                     break;
             }
         }
