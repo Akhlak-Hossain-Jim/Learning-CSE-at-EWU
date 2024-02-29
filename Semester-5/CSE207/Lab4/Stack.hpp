5317dd16cc15218b8088c70c4926c42fb5a16a83
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-02-29 23:36:52 +0600

    🏷️🛠️Stack created

---
+++ Semester-5/CSE207/Lab4/Stack.hpp
@@ -0,0 +1,101 @@
+// #include "../Lab2/Node.h"
+
+class Stack
+{
+private:
+    Node *head = NULL, *ref;
+    int length = 0;
+
+public:
+    Stack() {}
+
+    void Push(int value)
+    {
+        Node *neue = new Node(value);
+        if (head == NULL)
+        {
+            head = neue;
+            length++;
+        }
+        else
+        {
+            Node *temp = head;
+            head = neue;
+            neue->ref = temp;
+            length++;
+        }
+    }
+
+    void Pop()
+    {
+        if (head != NULL)
+        {
+            Node *temp = head;
+            head = head->ref;
+            this->length--;
+            delete temp;
+        }
+        else
+            cout << "No data found to remove";
+    }
+
+    int Length()
+    {
+        return this->length;
+    }
+
+    bool isEmpty()
+    {
+        return length == 0;
+    }
+
+    void destroy()
+    {
+        for (int i = 0; i < this->length; i++)
+        {
+            this->Pop();
+        }
+    }
+
+    int Top()
+    {
+        int res;
+        if (!(this->isEmpty()))
+        {
+            return this->head->item;
+        }
+        else
+        {
+            cout << "Can't provide you top value of an empty Stack";
+            return false;
+        }
+    }
+
+    Node *TopAddress()
+    {
+        Node *res;
+        if (!(this->isEmpty()))
+        {
+            return this->head;
+        }
+        else
+            cout << "Can't provide you top value of an empty Stack";
+    }
+
+    void display()
+    {
+        if (length > 0)
+        {
+
+            Node *temp = head;
+            while (temp != NULL)
+            {
+                cout << temp->item << "\t";
+                temp = temp->ref;
+            }
+            cout << endl;
+        }
+        else
+            cout << "Add some value to perform display\n";
+    }
+};
