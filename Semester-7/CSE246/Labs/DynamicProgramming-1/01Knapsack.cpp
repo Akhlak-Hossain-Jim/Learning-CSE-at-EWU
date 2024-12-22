e4885e3ce22c065fa48378fdaaddd674558408be
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-12-22 12:19:24 +0600

    🏷️🛠️added

---
+++ Semester-7/CSE246/Labs/DynamicProgramming-1/01Knapsack.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// 4
+// 2 3
+// 3 4
+// 4 5
+// 5 6
+// 8
+
+int maxV(int a, int b)
+{
+    if (a > b)
+        return a;
+    return b;
+}
+
+int main()
+{
+    int n, c;
+    cout << "Enter the length of items: ";
+    cin >> n;
+    int wt[n], p[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter wight for " << i + 1 << "th value: ";
+        cin >> wt[i];
+        cout << "Enter profit for " << i + 1 << "th value: ";
+        cin >> p[i];
+    }
+    cin >> c;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "\n\t\t" << i + 1 << "th weight: " << wt[i] << " & profit: " << p[i];
+    }
+    cout << "\n\n\t\tCapacity: " << c;
+
+    int DP[n + 1][c + 1];
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int w = 0; w <= c; w++)
+        {
+            if (i == 0 || w == 0)
+                DP[i][w] = 0;
+            else if (wt[i] > w)
+                DP[i][w] = DP[i - 1][w];
+            else
+                DP[i][w] = maxV(DP[i - 1][w], DP[i - 1][w - wt[i]] + p[i]);
+        }
+    }
+
+    cout << "\n\tThe Tabel:\n\n\t\t";
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= c; j++)
+        {
+            cout << DP[i][j] << "  ";
+        }
+        cout << "\n\t\t";
+    }
+
+    cout << "\n\n\t\tMax profit: " << DP[n][c];
+
+    const int refC = DP[n][c];
+    int ref = refC;
+    string res = "\n\n\n\t\t\tThe program will take:";
+
+    int i = n, j = c;
+    while (ref > 0)
+    {
+        cout << "\n---" << ref << "---" << i << j;
+        if (DP[i][j] == ref)
+        {
+            i--;
+        }
+        else if (DP[i][j] < ref)
+        {
+            res += " Obj " + to_string(i);
+            ref = ref - p[i];
+            ++i;
+            while (DP[i][j] != ref)
+            {
+                j--;
+            }
+        }
+        cout << "\n&&&" << ref << "---" << i << j;
+    }
+
+    cout << res;
+
+    return 0;
+}
---
+++ Semester-7/CSE246/Labs/DynamicProgramming-1/SumOfSubset.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+int sumofs(int n, int m, int value[])
+{
+    int dp[n + 1][m + 1];
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= m; j++)
+        {
+            if (i == 0 || j == 0)
+            {
+                dp[0][j] = 0;
+                dp[i][0] = 1;
+            }
+            else if (value[i] > j)
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+            else
+                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - value[i]];
+        }
+    }
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= m; j++)
+        {
+            cout << dp[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return dp[n][m];
+}
+
+int main()
+{
+    int n, sum;
+
+    cout << "Enter the length of items: ";
+    cin >> n;
+    int value[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter value for " << i + 1 << "th value: ";
+        cin >> value[i];
+    }
+    cin >> sum;
+
+    int t = sumofs(n, sum, value);
+    if (t == 1)
+    {
+        cout << "Yes";
+    }
+    else
+        cout << "No";
+}
