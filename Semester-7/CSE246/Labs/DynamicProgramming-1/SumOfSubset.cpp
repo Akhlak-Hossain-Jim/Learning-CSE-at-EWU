3e5573edcaf8f04cfa86b70907396616e08774bf
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2025-01-08 18:34:08 +0600

    🛠️fixes

--- Semester-7/CSE246/Labs/DynamicProgramming-1/SumOfSubset.cpp
+++ Semester-7/CSE246/Labs/DynamicProgramming-1/SumOfSubset.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 int sumofs(int n, int m, int value[])
 {
     int dp[n + 1][m + 1];
@@ -12,12 +13,12 @@ int sumofs(int n, int m, int value[])
                 dp[0][j] = 0;
                 dp[i][0] = 1;
             }
-            else if (value[i] > j)
+            else if (value[i - 1] > j)
             {
                 dp[i][j] = dp[i - 1][j];
             }
             else
-                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - value[i]];
+                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - value[i - 1]];
         }
     }
 
